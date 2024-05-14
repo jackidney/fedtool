cbab9efd61269b0922a9840534294821e71ee758
jackidney <jack.kidney@yale.edu>
2024-05-14 16:39:02 -0400

    Added a spreadsheet of the fomc meeting dates with realized plus anticipated rates. Started a product code generator.

--- file1.py
+++ file1.py
@@ -3,14 +3,38 @@
 Module Docstring
 """
 
-__author__ = "Your Name"
+__author__ = "Jack K"
 __version__ = "0.1.0"
 __license__ = "MIT"
+
+
+# Takes in the a month and year as numerics and spits out a proper product code of
+# ZQ Month Year
+def ProductCodeTwoNumbers(month, year):
+    my_dict = {
+    1: 'F'
+    2: 'G',
+    3: 'H',
+    4: 'J',
+    5: 'K',
+    6: 'M',
+    7: 'N',
+    8: 'Q',
+    9: 'U',
+    10: 'V',
+    11: 'X',
+    12: 'Z'
+}
+    return()
+
+
+
 
 
 def main():
     """ Main entry point of the app """
     print("hello world")
+
 
 
 if __name__ == "__main__":
