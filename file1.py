c102b772f2366a02baf74535ba05aa17b3e95c5d
jackidney <jack.kidney@yale.edu>
2024-05-16 10:20:22 -0400

    Started NearestMonth Function

--- file1.py
+++ file1.py
@@ -27,6 +27,14 @@ def ProductCodeTwoNumbers(month, year):
 }
     return("ZQ" + my_dict[month] + str(year))
 
+# Takes in a current
+def NearestMeeting(CurrentDate, prnt? = False):
+    if not isinstance(CurrentDate, list):
+        raise TypeError("Date must be in list = [day, month, year] format")
+    if len(CurrentDate) != 3:
+        raise ValueError("Date list must have size 3")
+    pass
+    
 
 def main():
     """ Main entry point of the app """
