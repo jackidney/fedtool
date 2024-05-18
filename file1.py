fa731ee090c5c39877e01c51e79a238e09475f4d
jackidney <jack.kidney@yale.edu>
2024-05-18 13:13:41 -0400

    Huzzah! The table now generates!

--- file1.py
+++ file1.py
@@ -49,35 +49,67 @@ def NearestNonMeetingMonth(CurrentDate, prnt=False):
     return (CurrentDate+relativedelta(months=2))
 
 def calculate(ReferenceDate = today, NumberOfMeetings = 2):
-    data = {
-    'Date': [],
-    'START': [],
-    'AVERAGE': [],
-    'END': [],
-    'Days': [],
-    '% Days': [],
-    'Price': [],
-    'Implied Rate': [],
-    'Monthly Change': [],
-    '# of 25bp Hikes': [],
-    '# Hikes Breakdown': []
-}
-    saved = datetime.date(2100,1,1)
-    for row in FOMCdf['FOMCDate']:
-        # Find the least row where FOMCDate is greater than or equal to CurrentDate
-        if ((row > ReferenceDate) & (row < saved)):
-            saved = row
-    print(saved)
+    data = pd.DataFrame(columns=[
+    'Date',
+    'START',
+    'AVERAGE',
+    'END',
+    'Days',
+    '% Days',
+    'Price',
+    'Implied Rate',
+    'Monthly Change',
+    '# of 25bp Hikes',
+    '# Hikes Breakdown'])
+
+    # Iterate over the rows of FOMCdf['FOMCDate'] and find the least row where FOMCDate is greater than or equal to ReferenceDate
+    saved = datetime.date(2050, 3, 3)
+    saved_index = None  
     
-    pass
+    for index, row in FOMCdf['FOMCDate'].iteritems():
+        if (row >= ReferenceDate) and (row < saved):
+            saved = row
+            saved_index = index
+    print(row, saved_index)
+    # Now get a list of the rows in FOMCdf['FOMCDate'] for saved_index to saved_index-NumberOfMeetings
+    rows_list = FOMCdf['FOMCDate'].iloc[(saved_index - NumberOfMeetings + 1):saved_index + 1].tolist()
+    print(rows_list)
+    print(max(rows_list))
 
+    
+    position = datetime.date(today.year, today.month, 1)
+    lst = list()
+    while(position < max(rows_list)):
+        lst.append(position)
+        print(position)
+        position = position+relativedelta(months=1)
+        print(position)
 
+    # REAL DEAL
+    for current in lst:
+        for _ in range(3):
+                date = current
+                start = 1 if _ == 0 else 0
+                average = 1 if _ == 1 else 0
+                end = 1 if _ == 2 else 0
+            
+                data = data.append({'Date': date, 'START': start, 'AVERAGE': average, 'END': end}, ignore_index=True)
+    print(data)
+    
+    def FillDays(frame):
+        
+
+    #for current in lst:
+        
+
+    
+    
     
 
 def main():
     """ Main entry point of the app """
     print("hello world")
-
+    calculate()
 
 
 if __name__ == "__main__":
